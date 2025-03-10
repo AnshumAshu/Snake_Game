53a0ad8c7d7c255ebc3c8e2987638f49b03a4d90
AnshumAshu <anshumashu2002@gmail.com>
2025-03-11 01:40:35 +0530

    third commit

--- game.js
+++ game.js
@@ -6,40 +6,40 @@ let lastRenderTime = 0
 let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
+window.onload = () => {
+    window.requestAnimationFrame(main);
+};
+
 function main(currentTime) {
-  if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) { //okay-true
-      window.location = '/'
+    if (gameOver) {
+        if (confirm('You lost. Press OK to restart.')) {
+            location.reload();
+        }
+        return;
     }
-    return
-  }
 
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
-  window.requestAnimationFrame(main)
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    lastRenderTime = currentTime;
 
-
-  lastRenderTime = currentTime
-
-  update()
-  draw()
+    update();
+    draw();
 }
 
-window.requestAnimationFrame(main)
-
 function update() {
-  updateSnake()
-  updateFood()
-  checkDeath()
+    updateSnake();
+    updateFood();
+    checkDeath();
 }
 
 function draw() {
-  gameBoard.innerHTML = ''
-  drawSnake(gameBoard)
-  drawFood(gameBoard)
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
 }
 
 function checkDeath() {
-  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
